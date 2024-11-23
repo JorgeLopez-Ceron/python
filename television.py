5adb295a80e4e5dca7f7096575d08576ced4758e
JorgeLopez-Ceron <jorge.lopezceron742@gmail.com>
2024-11-22 22:31:06 -0600

    added class var and __init__

--- television.py
+++ television.py
@@ -1,1 +1,15 @@
-print('test')
+
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+
+
