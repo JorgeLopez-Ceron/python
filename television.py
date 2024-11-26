8e0baf6841b444990c0b593f4a1374eee0e0c7d7
JorgeLopez-Ceron <jorge.lopezceron742@gmail.com>
2024-11-26 15:07:09 -0600

    added docstrings to methods

--- television.py
+++ television.py
@@ -1,14 +1,19 @@
 #add type hinting to methods
 
 class Television:
-    #class variables
+    """Class variables"""
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    #function to set instance values to defaults
     def __init__(self):
+        """function that sets the default values
+        Parameters: None
+
+        Returns: None
+
+        """
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
@@ -16,35 +21,68 @@ class Television:
 
     #turns tv on and off
     def power(self):
+        """turns the tv on and off
+
+        Parameters: None
+
+        Returns: None
+
+        """
         if not self.__status:
             self.__status = True
         elif self.__status:
             self.__status = False
-    #mutes and un mutes tv
+
     def mute(self):
+        """Mutes the tv
+
+        Parameters: None
+
+        Returns: None
+
+        """
         if not self.__muted:
             self.__muted = True
             self.__volume = 0
         elif self.__muted:
             self.__muted = False
 
-    #increases the channel
     def channel_up(self):
+        """increases the channel
+
+        Parameters: None
+
+        Returns: None
+
+        """
         if self.__status:
             if self.__channel == self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
             else:
                 self.__channel += 1
-    #decreases the channel
+
     def channel_down(self):
+        """turns the channel down
+
+        Parameters: None
+
+        Returns: None
+
+        """
         if self.__status:
             if self.__channel == self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    #increases volume
     def volume_up(self):
+        """turns the tv volume up
+
+        Parameters: None
+
+        Returns: None
+
+        """
         if self.__status:
             if self.__volume == self.MAX_VOLUME:
                 self.__volume == self.MAX_VOLUME
@@ -54,8 +92,14 @@ class Television:
         if self.__muted:
             self.__muted = False
 
-    #decreases volume
     def volume_down(self):
+        """turns the tv volume down
+
+        Parameters: None
+
+        Returns: None
+
+        """
         if self.__status:
             if self.__volume == self.MIN_VOLUME:
                 self.__volume == self.MIN_VOLUME
@@ -65,8 +109,14 @@ class Television:
         if self.__muted:
             self.__muted = False
 
-    #string that prints with tv info
     def __str__(self):
+        """ function that sets the print layout
+
+        Parameters: None
+
+        Returns: string: print statement that returns the values of status, channel, and volume
+
+        """
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
 # tests
